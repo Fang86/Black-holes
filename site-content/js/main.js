8cf8b79ed8d6d3f308e06607f7eb85310c915c09
Aidan Thiessen <aith1828@colorado.edu>
2022-04-21 17:45:52 -0600

    creategas and force update

--- site-content/js/main.js
+++ site-content/js/main.js
@@ -12,7 +12,15 @@ var config = {
         preload: preload,
         create: create,
         update: update
-    }
+    },
+    /*plugins: {
+        global: [{
+            key: 'rexSlider',
+            plugin: SliderPlugin,
+            start: true
+        },
+        ]
+    }*/
 };
 
 var game = new Phaser.Game(config);
@@ -20,7 +28,7 @@ var planet_a;
 var planet_b;
 var dbtext, dbtext2, dbtext3;
 var gConst = 1;
-var debug = true;
+var debug = false;
 var cursorKeys;
 var play = true;
 var r1;
@@ -28,6 +36,11 @@ var initSep = 140;
 var momentum;
 var solar_mass = 1;
 var solar_radius = 1;
+var density_limit = .001;
+var gasses = [];
+var mass_rate = .1;
+var grow_rate = .05;
+var scn;
 
 function preload ()
 {
@@ -38,11 +51,13 @@ function preload ()
     this.load.image('red', 'assets/sprites/red_ball.png');
     //this.load.image('blue', '../img/blue.png');
     this.load.scenePlugin('rexuiplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js', 'rexUI', 'rexUI');
+    this.load.plugin('rexsliderplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexsliderplugin.min.js', true);
 
 }
 
 function create ()
 {
+    scn = this
     this.physics.world.setBounds(0, 0, 2000, 2000);
     this.add.image(400, 300, 'bg');
     planet_a = this.add.sprite(400, 300, 'red');
@@ -51,23 +66,37 @@ function create ()
     dbtext2 = this.add.text(0, 25, '');
     dbtext3 = this.add.text(0, 50, '');
 
-    initPhys(planet_a, 0, 0, 5, 10);
+    initPhys(planet_a, 0, 0, 5, 5);
     initPhys(planet_b, 1.34, 0, 50, 65);
-    //planet_b.xVel = vc(planet_b, planet_a);
-    //planet_a.xVel = -vc(planet_b, planet_a);
 
-    //r1 = this.add.circle(200, 200, 25, 0xff2222);
+    // Accretion disk
+    ad_outer = this.add.circle(planet_a.x, planet_a.y, 34, 0xffff00, .5);
+    ad_mid = this.add.circle(planet_a.x, planet_a.y, 23, 0xff8800, .5);
+    ad_inner = this.add.circle(planet_a.x, planet_a.y, 15, 0xff0000, .5);
+
+    // Planet and roche lobe circles
     pl_a = this.add.circle(planet_a.x, planet_a.y, planet_a.radius, 0x000000);
-    rl_a = this.add.circle(planet_a.x, planet_a.y, 25, 0xffffff, .2);
     pl_b = this.add.circle(planet_b.x, planet_b.y, planet_b.radius, 0xff2222);
     rl_b = this.add.circle(planet_b.x, planet_b.y, 25, 0x000000, .4);
 
-    //planet_b.xAcc = 0;
-   
+    test = this.add.circle(planet_b.x, planet_b.y, 5, 0x000000, .4);
 
     cursorKeys = this.input.keyboard.createCursorKeys();
     cursorKeys.space.isOn = false;
     cursorKeys.shift.isOn = false;
+
+    //var slider = this.plugins.get('rexsliderplugin').add(gameObject, config);
+    /*var slider = scene.plugins.get('rexSlider').add(gameObject, {
+         endPoints: [
+             {x:0, y:0},
+             {x:200, y:100}
+         ],
+         value: 0,
+         enable: true,
+    
+        // valuechangeCallback: null,
+        // valuechangeCallbackScope: null
+    });*/
 }
 
 function update ()
@@ -81,13 +110,34 @@ function update ()
         motion(planet_b);
         //motion(planet_a);
 
-    //dbtext.setText(planet_b.mass + "TEST")
-    rl_b.radius = roche_radius(planet_b, planet_a);
-    rl_a.radius = roche_radius(planet_a, planet_b);
-    pl_b.radius = planet_b.radius
+        dbtext.setText("Radius (actual, calculated): " + planet_b.radius + ", " + circle_radius(planet_b.mass/planet_b.density))
+        dbtext2.setText(planet_b.mass + ", " + planet_b.density)
+        //dbtext.setText("Masses (density): " + planet_b.mass + " (" + planet_b.density + "), " + planet_a.mass)
+        rl_b.radius = roche_radius(planet_b, planet_a);
+        //rl_a.radius = roche_radius(planet_a, planet_b);
+        pl_b.radius = planet_b.radius
 
-    overflow(planet_b, planet_a);
-    age(planet_b);
+        if (planet_b.mass > 3) {
+            overflow(planet_b, planet_a);
+        }
+        if (planet_b.density > density_limit) {
+            age(planet_b);
+        }
+
+        //accrete(planet_a);
+        gasses.forEach(gas => {
+            orbit(gas, planet_a);
+            motion(gas);
+            //gas.xVel *= .999;
+            //gas.yVel *= .999;
+        });
+
+        let xDiff = planet_b.x - planet_a.x; // delta x
+        let yDiff = planet_b.y - planet_a.y; // delta y
+        let subAngle = -Math.atan(yDiff / xDiff) * 180/(Math.PI);
+        let angle = getAngle(xDiff, yDiff, subAngle) * Math.PI/180;
+        test.x = planet_b.x - planet_b.radius * Math.cos(angle)
+        test.y = planet_b.y + planet_b.radius * Math.sin(angle)
     }
 
     inputManager(this);
@@ -111,15 +161,13 @@ function orbit(bd, parent) // Gravity on bd by parent (acceleration)
 {
     var xDiff = bd.x - parent.x; // delta x
     var yDiff = bd.y - parent.y; // delta y
-    var disp = Math.pow(xDiff, 2) + Math.pow(yDiff, 2); // r^2
     var subAngle = -90;
-
     if (xDiff != 0) 
         subAngle = -Math.atan(yDiff / xDiff) * 180/(Math.PI);
-
     var angle = getAngle(xDiff, yDiff, subAngle);
+    var disp = Math.pow(xDiff, 2) + Math.pow(yDiff, 2); // r^2
     
-    bd.acc = (gConst * parent.mass * bd.mass) / (disp);
+    bd.acc = (gConst * parent.mass * 50) / (disp);
     bd.xAcc = bd.acc * Math.cos(angle * Math.PI/180);
     bd.yAcc = -bd.acc * Math.sin(angle * Math.PI/180);    
     
@@ -223,45 +271,43 @@ function inputManager(scn) // Handles inputs
     // Camera Movement
     if (cursorKeys.up.isDown)
     {
-        scn.cameras.main.y += 4;
+        mass_rate += .1
+        //scn.cameras.main.y += 4;
     }
     else if (cursorKeys.down.isDown)
     {
-        scn.cameras.main.y -= 4;
+        mass_rate -= .1
+        //scn.cameras.main.y -= 4;
     }
     if (cursorKeys.left.isDown)
     {
-        scn.cameras.main.x += 4;
+        grow_rate += .01
+        //scn.cameras.main.x += 4;
     }
     else if (cursorKeys.right.isDown)
     {
-        scn.cameras.main.x -= 4;
+        grow_rate -= .01
+        //scn.cameras.main.x -= 4;
     }
 }
 
 function overflow(donor, rec) 
 { // Simulates roche lobe overflow with donor star and recipient star
-    var mtr = 1
+    var mtr = mass_rate
     if (donor.radius > rl_b.radius) {
         var d_area = Math.PI * Math.pow(donor.radius, 2); // Donor area
         var r_area = Math.PI * Math.pow(rl_b.radius, 2); // Roche lobe area
-        //donor.density = donor.mass / d_area; // Donor density
         var of_area = d_area - r_area; // Overflow area
         var of_loss = of_area * donor.density; // Overflow mass loss
-        dbtext.setText(of_area + ", " + of_loss)
-        donor.mass -= mtr * of_loss;
-        rec.mass += mtr * of_loss;
-        //d_area = donor.mass / donor.density
-        //var init_rad = donor.radius
-        //d_area = 
-        donor.radius = Math.pow(r_area / Math.PI, 1/2);
-        //var d_mass = (donor.density * Math.PI * Math.pow(init_rad, 2)) - (donor.density * Math.PI * Math.pow(donor.radius, 2))
-        //donor.mass -= d_mass;
-        //rec.mass += d_mass;
-        console.log("dens, actual: " + donor.density + ", " + donor.mass / (Math.PI * Math.pow(donor.radius, 2)))
-        createGas(donor, of_area, of_loss)
+        if (donor.mass > mtr * of_loss) {
+            donor.mass -= mtr * of_loss;
+            rec.mass += mtr * of_loss;
+            donor.radius = circle_radius(donor.mass/donor.density) //Math.pow(r_area / Math.PI, 1/2);
+            createGas(donor, of_area, of_loss)
+        }
+
+        //console.log("dens, actual: " + donor.density + ", " + donor.mass / (Math.PI * Math.pow(donor.radius, 2)))
     }
-    
 }
 
 function roche_radius(donor, rec)
@@ -288,18 +334,48 @@ function vc(child, parent) // Velocity for circular orbit
 
 function age(bd) {
     var init_rad = bd.radius
-    var growth_const = .1;
+    var growth_const = grow_rate;
     bd.radius += growth_const;
     var init_area = Math.PI * Math.pow(init_rad, 2)
     var fin_area = Math.PI * Math.pow(bd.radius, 2)
     var d_area = fin_area - init_area;
+    //dbtext3.setText(bd.mass / fin_area + " - " + bd.mass / init_area + " = " + ((bd.mass / fin_area) - (bd.mass / init_area)))
     //bd.density -= bd.mass / (Math.PI * Math.pow(d_area, 2))
+    //bd.density = bd.mass / fin_area
 }
 
 function createGas(bd, area, mass) {
-    var mass_per = 10
-    var count = Math.floor(mass / mass_per);
+    var mass_per = .5
+    var count = 5 + 3 * Math.floor(mass / mass_per);
     var rem = mass % mass_per;
-    console.log("mass: " + mass + ", " + bd.mass)
-    console.log ("count, rem: " + count + ", " + rem)
+    //console.log("mass: " + mass + ", " + bd.mass)
+    //console.log ("count, rem: " + count + ", " + rem)
+
+    let xDiff = planet_b.x - planet_a.x; // delta x
+    let yDiff = planet_b.y - planet_a.y; // delta y
+    let subAngle = -Math.atan(yDiff / xDiff) * 180/(Math.PI);
+    let center = getAngle(xDiff, yDiff, subAngle) * Math.PI/180;
+    var angle;
+
+    for (let i = 0; i < count + 1; i++) {
+        angle = center + center * .01 * (i - Math.floor(count / 2))
+        gasses.push(scn.add.circle(planet_b.x - planet_b.radius * Math.cos(angle), planet_b.y + planet_b.radius * Math.sin(angle), 2, 0xff0000))
+        if (i==0) {
+            initPhys(gasses[gasses.length - 1], planet_b.xVel, planet_b.yVel, 100, 1.5 + rem * 3);
+        } else {
+            initPhys(gasses[gasses.length - 1], planet_b.xVel, planet_b.yVel, 100, 1.5 + mass_per * 3);
+        }
+    }
+}
+
+function circle_area(r) {
+    return Math.PI * Math.pow(r, 2);
+}
+
+function circle_radius(area) {
+    return Math.pow(area / Math.PI, 1/2);
+}
+
+function accrete(bd) {
+    return 1;
 }
